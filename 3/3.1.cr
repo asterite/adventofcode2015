6db3fbc16d125e4a5ca044330983e383e485de7f
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-12-03 07:39:10 -0300

    Day 3

---
+++ 3/3.1.cr
@@ -0,0 +1,20 @@
+# Since the file is small (8Kb) we use Int32, but we could use BigInt
+# (because the problem mentions an "infinite two-dimensional grid")
+x = 0
+y = 0
+
+visits = Set({Int32, Int32}).new
+visits << {0, 0}
+
+input = File.read("#{__DIR__}/input")
+input.each_char do |char|
+  case char
+  when '^' then y -= 1
+  when 'v' then y += 1
+  when '>' then x += 1
+  when '<' then x -= 1
+  end
+  visits << {x, y}
+end
+
+puts visits.size
---
+++ 3/3.2.cr
@@ -0,0 +1,34 @@
+class Santa
+  def initialize
+    @x = 0
+    @y = 0
+  end
+
+  def position
+    {@x, @y}
+  end
+
+  def move(char)
+    case char
+    when '^' then @y -= 1
+    when 'v' then @y += 1
+    when '>' then @x += 1
+    when '<' then @x -= 1
+    end
+  end
+end
+
+santa = Santa.new
+robo_santa = Santa.new
+
+visits = Set({Int32, Int32}).new
+visits << {0, 0}
+
+input = File.read("#{__DIR__}/input")
+input.each_char_with_index do |char, i|
+  current_santa = i.even? ? santa : robo_santa
+  current_santa.move(char)
+  visits << current_santa.position
+end
+
+puts visits.size
