448db004135f54c09369aad355236337f65fdaa8
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-12-10 10:13:08 -0300

    Day 10

---
+++ 10/10.1.cr
@@ -0,0 +1,21 @@
+def look_and_say(string)
+  last = nil
+  count = 0
+  String.build do |result|
+    string.each_char do |char|
+      if last && last != char
+        result << count << last
+        count = 0
+      end
+      last = char
+      count += 1
+    end
+    result << count << last
+  end
+end
+
+input = "1321131112"
+40.times do
+  input = look_and_say(input)
+end
+puts input.size
