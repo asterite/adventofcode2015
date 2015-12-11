25f01d129aaf7a30a77aa23806fc991a1f01593b
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-12-11 10:23:37 -0300

    Shorter 11.1 alternative using iterators

--- 11/11.1_alternative.cr
+++ 11/11.1_alternative.cr
@@ -1,12 +1,6 @@
-# Alternative solution inspired by https://www.reddit.com/r/adventofcode/comments/3wbzyv/day_11_solutions/cxv1vx5
-# (runs much slower than my original solution, but it's much more readable)
-stairs = [] of String
-('a'..'z').each_cons(3) do |chars|
-  stairs << chars.join
-end
-stairs = Regex.union(stairs)
+stairs = Regex.union(('a'..'z').each_cons(3).map(&.join).to_a)
 
-input = "hxbxxyzz"
+input = "hxbxwxba"
 loop do
   input = input.succ
   if input =~ stairs && !(input =~ /i|o|l/) && input.scan(/(\w)\1/).size > 1
