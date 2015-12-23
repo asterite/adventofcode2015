3f4e35d50986072ee30cf2eb24eeccaf55d2a183
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-12-23 02:37:35 -0300

    Day 23

---
+++ 23/23.2.cr
@@ -0,0 +1,34 @@
+input = File.read("#{__DIR__}/input")
+lines = input.lines
+
+reg = {"a" => 1, "b" => 0}
+i = 0
+while i < lines.size
+  line = lines[i]
+  case line
+  when /hlf (\w)/
+    reg[$1] /= 2
+  when /tpl (\w)/
+    reg[$1] *= 3
+  when /inc (\w)/
+    reg[$1] += 1
+  when /jmp ((?:\+|\-)\d+)/
+    i += $1.to_i
+    next
+  when /jie (\w), ((?:\+|\-)\d+)/
+    if reg[$1].even?
+      i += $2.to_i
+      next
+    end
+  when /jio (\w), ((?:\+|\-)\d+)/
+    if reg[$1] == 1
+      i += $2.to_i
+      next
+    end
+  else
+    raise "Unknown instruction: #{line}"
+  end
+
+  i += 1
+end
+puts reg["b"]
