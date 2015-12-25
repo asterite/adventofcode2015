bf4decd00863fb3db109a0701e553ea78494dc4b
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-12-25 02:53:21 -0300

    Simplification

--- 25/25.1.cr
+++ 25/25.1.cr
@@ -1,6 +1,5 @@
 col = 1
 row = 1
-next_row = 2
 value = 20151125_i64
 while true
   row -= 1
@@ -8,9 +7,8 @@ while true
   value = (value * 252533) % 33554393
   break if row == 2978 && col == 3083
   if row == 0
-    row = next_row
+    row = col
     col = 1
-    next_row += 1
   end
 end
 puts value
